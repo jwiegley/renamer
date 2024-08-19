e2b125f47395c908c9cfbcb517d95a9d40f1b536
John Wiegley <johnw@newartisans.com>
2024-08-19 14:47:43 -0700

    Code reorg, get ready to add tests

---
+++ bin/Main.hs
@@ -0,0 +1,192 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module Main where
+
+import Control.Lens hiding ((<.>))
+import Control.Monad (when)
+import Control.Monad.IO.Class
+import Data.Foldable (forM_)
+import Data.List (sort)
+import Data.Time
+import GHC.Conc (setNumCapabilities)
+import Options.Applicative hiding (command)
+import Options.Applicative qualified as OA
+import Renamer
+import System.Exit
+import System.FilePath
+import Text.Regex.TDFA.String ()
+import Text.Show.Pretty
+import Prelude hiding (putStrLn)
+
+version :: String
+version = "0.0.1"
+
+copyright :: String
+copyright = "2024"
+
+summary :: String
+summary =
+  "renamer "
+    ++ version
+    ++ ", (C) "
+    ++ copyright
+    ++ " John Wiegley"
+
+main :: IO ()
+main = do
+  opts <- getOptions
+  _ <- GHC.Conc.setNumCapabilities (opts ^. jobs)
+  errors <- runAppT opts $ do
+    case opts ^. command of
+      RenamePhotos dirs ->
+        renamePhotos dirs
+      ImportPhotos froms toPath dirs ->
+        importPhotos froms toPath dirs
+    use errorCount
+  if errors == 0
+    then exitSuccess
+    else exitWith (ExitFailure errors)
+  where
+    getOptions :: IO Options
+    getOptions = execParser optionsDefinition
+      where
+        optionsDefinition :: ParserInfo Options
+        optionsDefinition =
+          info
+            (helper <*> renamerOptions)
+            (fullDesc <> progDesc "" <> header summary)
+
+    renamerOptions :: Parser Options
+    renamerOptions =
+      Options
+        <$> switch
+          ( short 'q'
+              <> long "quiet"
+              <> help "Do not report any progress"
+          )
+        <*> switch
+          ( short 'v'
+              <> long "verbose"
+              <> help "Report progress verbosely"
+          )
+        <*> switch
+          ( short 'd'
+              <> long "debug"
+              <> help "Report progress at debug level"
+          )
+        <*> option
+          auto
+          ( short 'j'
+              <> long "jobs"
+              <> value 1
+              <> help "Number of concurrent IO jobs (default: 1)"
+          )
+        <*> switch
+          ( long "checksum"
+              <> help "Compute file checksums to detect duplicates"
+          )
+        <*> switch
+          ( long "execute"
+              <> help "Execute the renaming plan instead of just displaying it"
+          )
+        <*> switch
+          ( long "keep-state"
+              <> help "Keep state in .file-details.json (to aid debugging)"
+          )
+        <*> hsubparser
+          ( importCommand
+              <> renameCommand
+          )
+      where
+        importCommand :: Mod CommandFields Command
+        importCommand =
+          OA.command
+            "import"
+            (info importOptions (progDesc "Import photos"))
+          where
+            importOptions :: Parser Command
+            importOptions =
+              ImportPhotos
+                <$> some
+                  ( OA.strOption
+                      ( long "from"
+                          <> help "Entries to move into --to directory"
+                      )
+                  )
+                <*> OA.strOption
+                  ( long "to"
+                      <> help "Directory to move --from entries into"
+                  )
+                <*> some (OA.argument str (metavar "ENTRIES"))
+
+        renameCommand :: Mod CommandFields Command
+        renameCommand =
+          OA.command
+            "rename"
+            (info renameOptions (progDesc "Rename photos"))
+          where
+            renameOptions :: Parser Command
+            renameOptions =
+              RenamePhotos
+                <$> some (OA.argument str (metavar "ENTRIES"))
+
+buildAndExecutePlan :: Bool -> [FilePath] -> Maybe FilePath -> AppT IO ()
+buildAndExecutePlan gather dirs mdest =
+  doGatherDetails >>= doRenameFiles >>= doBuildPlan >>= doExecutePlan
+  where
+    doGatherDetails = do
+      putStrLn_ Normal "Gathering details..."
+      details <- sort <$> gatherDetails gather dirs
+      d <- view debug
+      when d $
+        forM_ details $ \det ->
+          putStrLn_ Debug $
+            det ^. filepath
+              ++ maybe "" ((" @ " ++) . show) (det ^. captureTime)
+      pure details
+
+    doRenameFiles details = do
+      putStrLn_ Normal $
+        "Determining expected file names (from "
+          ++ show (length details)
+          ++ " entries)..."
+      tz <- liftIO $ getTimeZone =<< getCurrentTime
+      renamings <- renameFiles tz details
+      d <- view debug
+      when d $
+        forM_ renamings $ \ren ->
+          putStrLn_ Debug $
+            ren ^. sourceDetails . filepath
+              ++ " >> "
+              ++ show (ren ^. renaming)
+      pure renamings
+
+    doBuildPlan renamings = do
+      putStrLn_ Normal $
+        "Building renaming plan (from "
+          ++ show (length renamings)
+          ++ " renamings)..."
+      plan <- buildPlan mdest renamings
+      v <- view verbose
+      d <- view debug
+      when d $
+        forM_ plan $ \(src, dst, ren) -> do
+          Just (srcPath, _) <- use (idxToFilepath . at src)
+          Just (dstPath, _) <- use (idxToFilepath . at dst)
+          putStrLn_ Debug $
+            srcPath ++ " >>> " ++ dstPath
+          when v $ liftIO $ pPrint ren
+      pure plan
+
+    doExecutePlan plan = do
+      tz <- liftIO $ getTimeZone =<< getCurrentTime
+      executePlan tz plan
+
+renamePhotos :: [FilePath] -> AppT IO ()
+renamePhotos = buildAndExecutePlan True ?? Nothing
+
+importPhotos :: [FilePath] -> FilePath -> [FilePath] -> AppT IO ()
+importPhotos froms toPath dirs = do
+  _ <- gatherDetails True dirs
+  buildAndExecutePlan False froms (Just toPath)
+  mapM_ safePruneDirectory froms
--- tests/Main.hs
+++ tests/Main.hs
@@ -2,11 +2,11 @@
 
 module Main where
 
-import Control.Lens
+-- import Control.Lens
 import Control.Monad.Trans.Class
 import Hedgehog hiding (Action)
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
+-- import qualified Hedgehog.Gen as Gen
+-- import qualified Hedgehog.Range as Range
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.Hedgehog
@@ -17,5 +17,5 @@ main =
     testGroup
       "renamer"
       [ testProperty "zipper-unzipper" $ property do
-          pure $ () @?= ()
+          lift $ () @?= ()
       ]
